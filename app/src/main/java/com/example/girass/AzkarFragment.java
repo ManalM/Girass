565fe456ef935797e812949582a2339cff5cee54
ManalM <mmsaj000@hotmail.com>
2020-03-11 05:58:31 +0300

    final project

--- app/src/main/java/com/example/girass/AzkarFragment.java
+++ app/src/main/java/com/example/girass/AzkarFragment.java
@@ -1,6 +1,8 @@
 package com.example.girass;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -19,17 +21,17 @@ import android.view.LayoutInflater;
 
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.*:
 
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-import androidx.appcompat.widget.SearchView;
 
 import android.widget.TextView;
 
@@ -72,6 +74,7 @@ public class AzkarFragment extends Fragment implements Adapter.SelectedUser {
         TextView toolbarText = rootView.findViewById(R.id.toolbar_title);
 
 
+
         toolbar.setTitle("");
         toolbarText.setText(R.string.azkar);
 
@@ -83,25 +86,8 @@ public class AzkarFragment extends Fragment implements Adapter.SelectedUser {
         ///////////////////////////////////////////
 
         listView = rootView.findViewById(R.id.list);
-        DataService dataService = new DataService();
-        final HeadZikrObject[] headZikrObjects = dataService.GetAllAzkar();
 
-        titles = new ArrayList<String>();
-
-        int i = 0;
-        while (i < headZikrObjects.length) {
-            titles.add(headZikrObjects[i].TITLE);
-
-            i++;
-        }
-
-
-
-        mAdapterAzkar = new Adapter(getContext(), titles, this);
-        listView.setLayoutManager(new GridLayoutManager(getContext(), 1));
-        listView.setAdapter(mAdapterAzkar);
-
-
+        createArray();
         ///----------------search btn and process ----------
 
         search.addTextChangedListener(new TextWatcher() {
@@ -126,7 +112,9 @@ public class AzkarFragment extends Fragment implements Adapter.SelectedUser {
             public void onClick(View v) {
                 titles.clear();
                 toolbar.setVisibility(View.GONE);
+
                 searchBar.setVisibility(View.VISIBLE);
+                keyboardShowAndHide(getActivity(), search, 0);
 
             }
         });
@@ -136,11 +124,10 @@ public class AzkarFragment extends Fragment implements Adapter.SelectedUser {
             public void onClick(View v) {
                 toolbar.setVisibility(View.VISIBLE);
                 searchBar.setVisibility(View.GONE);
-                int i = 0;
-                while (i < headZikrObjects.length) {
-                    titles.add(headZikrObjects[i].TITLE);
-                    i++;
-                }
+
+                createArray();
+
+                keyboardShowAndHide(getActivity(), search, 1);
             }
         });
 
@@ -150,9 +137,39 @@ public class AzkarFragment extends Fragment implements Adapter.SelectedUser {
     }
 
 
+    private void createArray() {
+        DataService dataService = new DataService();
+        final HeadZikrObject[] headZikrObjects = dataService.GetAllAzkar();
+
+        titles = new ArrayList<String>();
+
+        int i = 0;
+        while (i < headZikrObjects.length) {
+            titles.add(headZikrObjects[i].TITLE);
+
+            i++;
+        }
 
 
+        mAdapterAzkar = new Adapter(getContext(), titles, this);
+        listView.setLayoutManager(new GridLayoutManager(getContext(), 1));
+        listView.setAdapter(mAdapterAzkar);
+    }
 
+    private void keyboardShowAndHide(Activity activity, EditText editText, int value) {
+        editText.requestFocus();
+        try {
+
+            InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (value == 0)
+                imm.showSoftInput(search, InputMethodManager.SHOW_IMPLICIT);
+            else
+                imm.hideSoftInputFromWindow(search.getWindowToken(), 0);
+        } catch () {
+
+        }
+
+    }
     ///---------- send data to allZIkr fragment ------------
     @Override
     public void selectedUser(String s) {
@@ -185,7 +202,6 @@ public class AzkarFragment extends Fragment implements Adapter.SelectedUser {
 
         currentVisiblePosition = 0;
     }
-
 
 
 }
